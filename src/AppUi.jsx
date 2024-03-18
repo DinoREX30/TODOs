73fb7813bca59872ef3cacb24d10058ff6d06d7e
Raúl <baltazardominguez.raul@outlook.com>
2024-03-18 00:43:44 -0600

    Title added

--- src/AppUi.jsx
+++ src/AppUi.jsx
@@ -3,6 +3,8 @@ import TodoItem from './components/TodoItem'
 import TodoList from './components/TodoList'
 import TodoSearch from './components/TodoSearch'
 import TodoTittle from './components/TodoTitttle'
+import { Fade } from "react-awesome-reveal";
+
 
 export default function AppUi({
     completedTodos,
@@ -12,34 +14,35 @@ export default function AppUi({
     searchedTodos,
     completeTodo,
     deleteTodo,
-    
+
 }) {
   return (
     <>
-    <div className='flex h-screen flex-col lg:flex-row xl:flex-row'>
-      <section className='first content-center h-90 flex-col'>
-        <h1 className='text-center font-thin text-2xl'>Crea una nueva tarea</h1>
-        <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue} 
-        />
-        <CreateTodoButton/>
-      </section>
-      <section className='h-auto'>
-      <TodoTittle completed={completedTodos} total={totalTodos}/>
-        <TodoList>
-          {searchedTodos.map(todo => (
-            <TodoItem 
-            key={todo.text} 
-            text={todo.text} 
-            completed={todo.completed}
-            //Forma de pasarle una funcion a un componete sin ejecutarla
-            onComplete={() =>  completeTodo(todo.text)}
-            onDelete={() =>  deleteTodo(todo.text)}
-          />
-          ))}
-        </TodoList>
-      </section>
+    <h1 className='my-20 text-3xl font-thin bg-black text-white flex justify-center p-4 rounded-lg relative max-w-xl mx-auto'>WELCOME TO  MY TODO APP</h1>
+    <div className='flex flex-col lg:flex-row xl:flex-row'>
+        <section className='first content-center h-90 flex-col'>
+            <h1 className='text-center font-thin text-2xl'>Crea una nueva tarea</h1>
+            <TodoSearch 
+            searchValue={searchValue}
+            setSearchValue={setSearchValue} 
+            />
+            <CreateTodoButton/>
+        </section>
+        <section className='h-auto'>
+        <TodoTittle completed={completedTodos} total={totalTodos}/>
+            <TodoList>
+            {searchedTodos.map(todo => (
+                <TodoItem 
+                key={todo.text} 
+                text={todo.text} 
+                completed={todo.completed}
+                //Forma de pasarle una funcion a un componete sin ejecutarla
+                onComplete={() =>  completeTodo(todo.text)}
+                onDelete={() =>  deleteTodo(todo.text)}
+            />
+            ))}
+            </TodoList>
+        </section>
     </div>
     </>
   )
